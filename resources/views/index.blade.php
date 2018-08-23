ea8bb457789f70435847fcd71c62a5c02350730f
Leandro Roberto Home <leroberto@gmail.com>
2018-08-22 23:27:33 -0300

    HackDay first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.startbootstrap')
+@section('content')
+
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">Home</h1>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12">
+        Hack Day!
+    </div>
+</div>
+
+
+@endsection
